c032cb1e504826355b40443f02cc422d8553b9e6
Jose Hurtado <AlguienMore@users.noreply.github.com>
2019-04-27 20:12:07 -0500

    Implementacion de WebSocket

--- src/screens/post/index.js
+++ src/screens/post/index.js
@@ -7,61 +7,69 @@ import * as firebase from 'firebase';
 
 import * as cons from '../../res/values/constants'
 // import 'typeface-roboto';
+
+let results = [];
 class Post extends React.Component {
-      /*function get_post_desc() {
-        return (
+    /*function get_post_desc() {
+      return (
 
-        );
+      );
 
-    }*/
-    constructor(props){
-      super(props);
-      cons.serverStart();
-      this.state = {
-        multiline:'',
-          data : null
-      }
+  }*/
+    constructor(props) {
+        super(props);
+        cons.serverStart();
+        this.state = {
+            multiline: '',
+            name_des: '',
+            object_lov: {},
+            data: null
+        }
     }
+
     componentDidMount() {
-        const { match: { params } } = this.props;
-        firebase.database().ref('/post/').on("value", snapshot => {
-        /*    results = snapshot.val(); //siempre es snapshot.val() para tomar el json de la ruta
+        const {match: {params}} = this.props;
+        firebase.database().ref(`/post/${params.id}`).on("value", snapshot => {
+            results = snapshot.val(); //siempre es snapshot.val() para tomar el json de la ruta
             console.log(snapshot.val());
             console.log(results)//esto es para mostrar
-            let xd = Object.getOwnPropertyNames(results);
-            this.setState({dataKeys: xd, getFckanSht: results});*/
+            this.setState({object_lov: results});
+
         });
     }
 
     handleChange = name => event => {
-        this.setState({ [name]: event.target.value });
+        this.setState({[name]: event.target.value});
     };
 
     render() {
         cons.webSocket.on('result', (arraytemp) => {
-            this.setState({data: arraytemp });
+            this.setState({data: arraytemp});
         });
         cons.webSocket.on('result-item', (arraytemp) => {
-            this.setState({keysThings: arraytemp });
+            this.setState({keysThings: arraytemp});
         });
         return (
 
-          <div className="post">
-            <div class="post_description"><h3>Titulo y descripcion del card seleccionado de home</h3></div>
-            <TextField
-             id="outlined-multiline-flexible"
-             label="Post"
-             multiline
-             rowsMax="8"
-             value={this.state.multiline}
-             onChange={this.handleChange('multiline')}
-             className={this.props.textField}
-             margin="normal"
-             helperText="hello"
-             variant="outlined"
-             />
-            {/*<Typography>{this.state.multiline}</Typography>*/}
-          </div>
+            <div className="post">
+                <div class="post_description"><h3>{this.state.object_lov.titulo}</h3></div>
+                <p>{this.state.object_lov.descripcion}</p>
+
+                <TextField
+                    id="outlined-multiline-flexible"
+                    label="Post"
+                    multiline
+                    rowsMax="8"
+                    value={this.state.multiline}
+                    onChange={this.handleChange('multiline')}
+                    className={this.props.textField}
+                    margin="normal"
+                    helperText="hello"
+                    variant="outlined"
+                />
+
+                {/*<Typography>{this.state.multiline}</Typography>*/}
+            </div>
 
         );
 
