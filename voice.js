2496bf83b9b4a205925d7a8679d3d98ae5eb77fa
Bxtrd <bryane@uninorte.edu.co>
2019-04-27 23:41:43 -0500

    voices 2

--- voice.js
+++ voice.js
@@ -1,17 +1,25 @@
-function readOutLoud(message) {
-    var speech = new SpeechSynthesisUtterance();
+document.addEventListener("keydown",fun);
+			var tab = 9;
+			function fun(event){
+				console.log("tecla presionada ",event.keyCode);
+				console.log("tabular");
+				var x = document.activeElement.innerText.toString();
+				console.log(x);
+				
+				readOutLoud(x);
 
-    // Set the text and voice attributes.
-    speech.text = message;
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 1;
-    voices = window.speechSynthesis.getVoices()
-    speech.voice = voices[0];
-    speech.lang = voices[0].lang;
+			}
+			function readOutLoud(message) {
+    			var speech = new SpeechSynthesisUtterance();
 
-    window.speechSynthesis.speak(speech);
-    }
+		    	// Set the text and voice attributes.
+    			speech.text = message;
+    			speech.volume = 1;
+    			speech.rate = 1;
+    			speech.pitch = 1;
+    			voices = window.speechSynthesis.getVoices()
+   				speech.voice = voices[0];
+    			speech.lang = voices[0].lang;
 
-    var texto = "zawaaarudo";
-    readOutLoud(texto);
+    			window.speechSynthesis.speak(speech);
+   			}
